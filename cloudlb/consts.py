4470f1d684a1b3b2a77f13e6fb8c044c1f61ed4a
joe miller <joeym@joeym.net>
2012-12-12 01:06:51 +0000

    bumped version so our chef recipe can deal with this efficiently

--- cloudlb/consts.py
+++ cloudlb/consts.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 __author__ = "Chmouel Boudjnah <chmouel@chmouel.com>"
-VERSION = "0.5.1"
+VERSION = "0.5.2"
 USER_AGENT = 'python-cloudb/%s' % VERSION
 
 # Default AUTH SERVER
